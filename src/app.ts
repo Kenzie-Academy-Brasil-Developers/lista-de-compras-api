163b08a1d47b36bbe323deb4c71a682ceb54bd5f
Vitor Galdino <httpvittor@gmail.com>
2023-01-25 20:11:49 -0300

    feat: created app delete request with deleteList how callback

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express';
 
-import { createList, deleteListItem, getList, getListById } from './logic';
+import { createList, deleteList, deleteListItem, getList, getListById } from './logic';
 
 const app: Application = express();
 app.use(express.json());
@@ -9,6 +9,7 @@ app.post('/purchaseList', createList);
 app.get('/purchaseList', getList);
 app.get('/purchaseList/:id', getListById);
 app.delete('/purchaseList/:listId/:itemName', deleteListItem);
+app.delete('/purchaseList/:listId', deleteList);
 
 app.listen(3000, () => {
     console.log('Server is running! 🚀');
