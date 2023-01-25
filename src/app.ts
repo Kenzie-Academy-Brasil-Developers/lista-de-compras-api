3ce5a12c2311cefda424bf1c0e491d0adec82c19
Vitor Galdino <httpvittor@gmail.com>
2023-01-25 17:56:37 -0300

    feat: created get request to getListById

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
-import { createList, getList } from './logic';
+
+import { createList, getList, getListById } from './logic';
 
 const app: Application = express();
 app.use(express.json());
 
 app.post('/purchaseList', createList);
 app.get('/purchaseList', getList);
+app.get('/purchaseList/:id', getListById);
 
 app.listen(3000, () => {
     console.log('Server is running! 🚀');
