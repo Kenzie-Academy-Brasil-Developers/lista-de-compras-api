4567d80096747a6011403d36df72397719c0cd13
Vitor Galdino <httpvittor@gmail.com>
2023-01-25 12:09:57 -0300

    feat: included post and get method to api

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Application } from 'express';
+import { createList, getList } from './logic';
 
 const app: Application = express();
 app.use(express.json());
 
+app.post('/purchaseList', createList);
+app.get('/purchaseList', getList);
+
 app.listen(3000, () => {
-    console.log('Server is running!');
+    console.log('Server is running! 🚀');
 });
