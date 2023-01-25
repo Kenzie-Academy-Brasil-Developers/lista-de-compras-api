4827b2b838eb30a5083279c4e7a0294b3902c414
Vitor Galdino <httpvittor@gmail.com>
2023-01-25 17:29:09 -0300

    feat: created createList logic function

--- src/logic.ts
+++ src/logic.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
+
 import { internalData } from './database';
+import { IOrderList, IOrderListRequest } from './interfaces';
 
-export const createList = (req: Request, res: Response) => {
+export const createList = (req: Request, res: Response): Response => {
+    const orderData: IOrderListRequest = req.body;
+    const id: number = Math.floor(Math.random() * 1000);
 
+    const newOrderData: IOrderList = {
+        ...orderData,
+        id,
+    };
+
+    internalData.push(newOrderData);
+
+    return res.status(201).json(newOrderData);
 };
 
-export const getList = (req: Request, res: Response) => {
+export const getList = (req: Request, res: Response): Response => {
     return res.json(internalData);
 };
