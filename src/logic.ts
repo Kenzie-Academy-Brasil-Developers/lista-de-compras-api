af0d4445bf52c2a32567b21194223acaf82349c4
Vitor Galdino <httpvittor@gmail.com>
2023-01-25 12:13:41 -0300

    feat: created return to getList function

--- src/logic.ts
+++ src/logic.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
+import { internalData } from './database';
 
 export const createList = (req: Request, res: Response) => {
 
 };
 
 export const getList = (req: Request, res: Response) => {
-
+    return res.json(internalData);
 };
