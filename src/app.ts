fcc987b90558ea643b382e188cc1018a65a22daf
Vitor Galdino <httpvittor@gmail.com>
2023-01-26 00:14:17 -0300

    feat: created app patch request with updateListItem callback

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express';
 
-import { createList, deleteList, deleteListItem, getList, getListById } from './logic';
+import { createList, deleteList, deleteListItem, getList, getListById, updateListItem } from './logic';
 
 const app: Application = express();
 app.use(express.json());
@@ -10,7 +10,8 @@ app.get('/purchaseList', getList);
 app.get('/purchaseList/:id', getListById);
 app.delete('/purchaseList/:listId/:itemName', deleteListItem);
 app.delete('/purchaseList/:listId', deleteList);
+app.patch('/purchaseList/:listId/:itemName', updateListItem);
 
 app.listen(3000, () => {
-    console.log('Server is running! 🚀');
+    console.log('Server is running on http://localhost:3000/ 🚀');
 });
